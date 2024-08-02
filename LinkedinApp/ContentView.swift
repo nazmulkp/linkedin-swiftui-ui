b1673a739b20faa34d5597c41b5f176db0acdb18
Nazmul <nazmul.hasan@monstar-lab.com>
2024-08-02 16:36:29 +0600

    Initial Commit

---
+++ LinkedinApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LinkedinApp
+//
+//  Created by Md. Nazmul Hasan on 2/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LinkedinApp/LinkedinAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinAppApp.swift
+//  LinkedinApp
+//
+//  Created by Md. Nazmul Hasan on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
